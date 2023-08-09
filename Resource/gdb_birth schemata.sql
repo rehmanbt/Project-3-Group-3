32ccd407944273396b44443b2ff63a9a8bfe1bca
rehmanbt <130420887+rehmanbt@users.noreply.github.com>
2023-08-09 19:54:39 -0400

    updating after team review

---
+++ Resource/gdb_birth schemata.sql
@@ -0,0 +1,13 @@
+CREATE TABLE birth_rate (
+    Country TEXT,
+    Country_Code TEXT,
+    Year INTEGER,
+    Birth_Rate FLOAT
+);
+
+CREATE TABLE yearly_gdp (
+    Country TEXT,
+    Country_Code TEXT,
+    Year INTEGER,
+    GDP FLOAT
+);
